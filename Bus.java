49ddf591fceacab4c9997ba7b9c88f9c8307d7e6
Muhammad Shidqi Hanif <shidqi24@mhs.usk.ac.id>
2025-12-01 14:52:00 +0700

    Membuat method getter untuk mendapatkan info dari atribut dasar class Bus di file Bus.java

--- Bus.java
+++ Bus.java
@@ -20,4 +20,39 @@ public class Bus {
     penumpangBerdiri = new ArrayList<>(); // Inisialisasi ArrayList penumpang berdiri
     totalPendapatan = 0; // Inisialisasi total pendapatan bus dengan nilai
   }
+
+  // Method Getter untuk mendapatkan total penumpang biasa
+  public ArrayList<Penumpang> getPenumpangBiasa(){
+    return this.penumpangBiasa; // Mengembalikan ArrayList penumpang biasa
+  }
+
+  // Method Getter untuk mendapatkan total penumpang prioritas
+  public ArrayList<Penumpang> getPenumpangPrioritas(){
+    return this.penumpangPrioritas; // Mengembalikan ArrayList penumpang prioritas
+  }
+
+  // Method Getter untuk mendapatkan total penumpang berdiri
+  public ArrayList<Penumpang> getPenumpangBerdiri(){
+    return this.penumpangBerdiri; // Mengembalikan ArrayList penumpang berdiri
+  }
+
+  // Method Getter untuk mendapatkan jumlah total penumpang biasa
+  public int getJumlahPenumpangBiasa(){
+    return this.penumpangBiasa.size(); // Mengembalikan jumlah total penumpang biasa dengan method size() dari ArrayList
+  }
+
+  // Method Getter untuk mendapatkan jumlah total penumpang prioritas
+  public int getJumlahPenumpangPrioritas(){
+    return this.penumpangPrioritas.size(); // Mengembalikan jumlah total penumpang prioritas dengan method size() dari ArrayList
+  }
+
+  // Method Getter untuk mendapatkan jumlah total penumpang berdiri
+  public int getJumlahPenumpangBerdiri(){
+    return this.penumpangBerdiri.size(); // Mengembalikan jumlah total penumpang berdiri dengan method size() dari ArrayList
+  }
+
+  // Method Getter untuk mendapatkan total pendapatan bus
+  public int getTotalPendapatan(){
+    return this.totalPendapatan; // Mengembalikan total pendapatan bus
+  }
 }
