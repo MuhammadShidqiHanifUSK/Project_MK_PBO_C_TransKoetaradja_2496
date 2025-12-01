c58016592d0f1cc813d2821860c2d29c2288947c
Muhammad Shidqi Hanif <shidqi24@mhs.usk.ac.id>
2025-12-01 12:17:25 +0700

    Membuat atribut dasar pada class Penumpang

--- Penumpang.java
+++ Penumpang.java
@@ -1,3 +1,7 @@
 public class Penumpang {
-  
+  // Atribut dasar Penumpang
+  private int id; // ID unik untuk setiap penumpang pada Bus Trans Koetaradja
+  private int umur; // Umur penumpang dalam satuan integer tahun
+  private boolean hamil; // Status kehamilan penumpang, True = Penumpang hamil, False = Penumpang tidak hamil
+  private int saldo; // Saldo uang yang dimiliki penumpang
 }
