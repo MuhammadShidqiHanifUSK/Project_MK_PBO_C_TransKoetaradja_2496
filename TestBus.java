43e7b0303354f34a4b0cbbd353a38ed2070a4703
Muhammad Shidqi Hanif <shidqi24@mhs.usk.ac.id>
2025-12-08 15:25:01 +0700

    Persiapan awal TestBus.java

--- TestBus.java
+++ TestBus.java
@@ -1,3 +1,12 @@
-public class Main {
-  
+import java.util.Random;
+import java.util.Scanner;
+
+public class TestBus{
+  private static Bus bus = new Bus();
+  private static Scanner scanner = new Scanner(System.in);
+  private static Random random = new Random();
+  private static int idCounter = 1;
+  public static void main(String[] args) {
+    
+  }
 }
